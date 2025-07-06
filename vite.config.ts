bcd2c8f6afb57b3709596454394bc3bbde65c1e6
AjiNurAji <ajinuraji090306@gmail.com>
2025-07-06 10:00:59 +0700

    enable to public local network

--- vite.config.ts
+++ vite.config.ts
@@ -2,24 +2,33 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { resolve } from 'node:path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
-            ssr: 'resources/js/ssr.tsx',
-            refresh: true,
-        }),
-        react(),
-        tailwindcss(),
+      laravel({
+        input: ['resources/css/app.css', 'resources/js/app.tsx'],
+        ssr: 'resources/js/ssr.tsx',
+        refresh: true,
+      }),
+      react(),
+      tailwindcss(),
     ],
     esbuild: {
-        jsx: 'automatic',
+      jsx: 'automatic',
     },
     resolve: {
-        alias: {
-            'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
-        },
+      alias: {
+        'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
+      },
     },
+    server: {
+      host: env.VITE_DEV_HOST || '127.0.0.1',
+      port: 5173,
+      strictPort: true,
+    }
+  }
 });
