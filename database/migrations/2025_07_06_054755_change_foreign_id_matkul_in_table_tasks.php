8b908edf6467b9506b0cef1615282f124c800142
AjiNurAji <ajinuraji090306@gmail.com>
2025-07-06 19:40:02 +0700

    change and add column

---
+++ database/migrations/2025_07_06_054755_change_foreign_id_matkul_in_table_tasks.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropColumn('id_matkul');
+            $table->foreignUuid('id_matkul')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropColumn('id_matkul');
+        });
+    }
+};
