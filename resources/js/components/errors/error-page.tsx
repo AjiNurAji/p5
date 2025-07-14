793b6f9a280344ea742bc00e808fdc03276873b6
AjiNurAji <ajinuraji090306@gmail.com>
2025-07-14 12:48:51 +0700

    fix error page style

--- resources/js/components/errors/error-page.tsx
+++ resources/js/components/errors/error-page.tsx
@@ -7,34 +7,42 @@ export const ErrorPage = ({
   code = 500,
   error = "Wah! Ada yang salah nih.",
   message,
-  className
+  className,
+  withDashBtn = false
 }: {
   code?: number;
   error?: string;
   message?: React.ReactNode;
   className?: string;
+  withDashBtn?: boolean
 }) => {
   return (
-    <div className={cn('h-full', className)}>
+    <div className={cn("h-full", className)}>
       <Head title={code.toString()} />
-      <div className='m-auto flex h-full w-full flex-col items-center justify-center gap-2'>
-        <h1 className='text-[7rem] leading-tight font-bold'>{code}</h1>
-        <span className='font-medium'>{error}</span>
-        <p className='text-muted-foreground text-center'>
-          {message ?? <>Kami mohon maaf atas ketidaknyamanan ini. <br /> Silakan coba lagi nanti.</>}
-        </p>
+      <div className="m-auto flex h-full w-full flex-col items-center justify-center gap-2">
+        <h1 className="text-[7rem] leading-tight font-bold">{code}</h1>
+        <span className="font-medium">{error}</span>
+        <div className="text-center text-muted-foreground">
+          {message ?? (
+            <p>
+              Kami mohon maaf atas ketidaknyamanan ini. <br /> Silakan coba lagi
+              nanti.
+            </p>
+          )}
+        </div>
         {code !== 503 && code !== 500 && (
-          (<div className='mt-6 flex gap-4'>
-            <Button variant='outline' onClick={() => history.go(-1)}>
+          <div className="mt-6 flex gap-4">
+            <Button variant="outline" onClick={() => history.go(-1)}>
               Kembali
             </Button>
-            <Button asChild>
-              <Link href={route('dashboard')}>
-                Kembali ke Dashboard</Link>
-            </Button>
-          </div>)
+            {withDashBtn && (
+              <Button asChild>
+                <Link href={route("dashboard")}>Kembali ke Dashboard</Link>
+              </Button>
+            )}
+          </div>
         )}
       </div>
     </div>
-  )
-}
+  );
+};
--- resources/js/pages/user/users.tsx
+++ resources/js/pages/user/users.tsx
@@ -3,22 +3,25 @@ import Heading from "@/components/heading";
 import AppLayout from "@/layouts/app-layout";
 import { BreadcrumbItem, SharedData } from "@/types";
 import { Head, usePage } from "@inertiajs/react";
+import { userListSchema } from "./components/data/schema";
+import { columns } from "./components/tables/user-column";
+import { UsersTable } from "./components/tables/user-table";
+import { UserDialogs } from "./components/users-dialogs";
 import { UsersPrimaryButtons } from "./components/users-primary-buttons";
 import UsersProvider from "./context/users-context";
-import { UserDialogs } from "./components/users-dialogs";
-import { UsersTable } from "./components/tables/user-table";
-import { columns } from "./components/tables/user-column";
-import { userListSchema } from "./components/data/schema";
 
 const breadcrumbs: BreadcrumbItem[] = [
   {
-    title: 'Pengguna',
-    href: route('users.index'),
+    title: "Pengguna",
+    href: route("users.index"),
   },
 ];
 
 const UsersPage = () => {
-  const { users, auth: { user } } = usePage<SharedData>().props;
+  const {
+    users,
+    auth: { user },
+  } = usePage<SharedData>().props;
 
   const userList = userListSchema.parse(users);
 
@@ -26,26 +29,37 @@ const UsersPage = () => {
     <AppLayout breadcrumbs={breadcrumbs}>
       <Head title="Pengguna" />
       <UsersProvider>
-        {user.role === 'member' ?
-          <ErrorPage code={403} error="Akses Dibatasi!" message="Upss! Anda tidak memiliki izin <br />
-                    untuk melihat halaman ini." />
-          :
-          (
-            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
-              <div className='mb-2 flex flex-wrap items-center justify-between space-y-2'>
-                <Heading title="Daftar Pengguna" description="Atur pengguna dan hak akses mereka dengan mudah di halaman ini." />
-                <UsersPrimaryButtons />
-              </div>
-              <div className='-mx-4 flex-1 overflow-auto px-4 py-1 lg:flex-row lg:space-y-0 lg:space-x-12'>
-                <UsersTable data={userList} columns={columns} />
-              </div>
+        {user.role === "superadmin" ? (
+          <ErrorPage
+            withDashBtn
+            code={403}
+            error="Akses Dibatasi!"
+            message={
+              <p>
+                Upss! Anda tidak memiliki izin <br />
+                untuk melihat halaman ini.
+              </p>
+            }
+          />
+        ) : (
+          <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
+            <div className="mb-2 flex flex-wrap items-center justify-between space-y-2">
+              <Heading
+                title="Daftar Pengguna"
+                description="Atur pengguna dan hak akses mereka dengan mudah di halaman ini."
+              />
+              <UsersPrimaryButtons />
             </div>
-          )}
+            <div className="-mx-4 flex-1 overflow-auto px-4 py-1 lg:flex-row lg:space-y-0 lg:space-x-12">
+              <UsersTable data={userList} columns={columns} />
+            </div>
+          </div>
+        )}
 
         <UserDialogs />
       </UsersProvider>
     </AppLayout>
-  )
-}
+  );
+};
 
 export default UsersPage;
