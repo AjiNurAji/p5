4df0a9a736af74f1f2c6bb5af7ae9b0de2038a9f
AjiNurAji <ajinuraji090306@gmail.com>
2025-07-28 18:18:37 +0530

    send email when update profile

--- app/Http/Controllers/Settings/ProfileController.php
+++ app/Http/Controllers/Settings/ProfileController.php
@@ -35,6 +35,10 @@ class ProfileController extends Controller
     if ($request->user()->isDirty("email")) {
       $request->user()->email_verified_at = null;
     }
+    
+    if ($request->input("email")) {
+        $request->user()->sendEmailVerificationNotification();
+    }
 
     $request->user()->save();
 
