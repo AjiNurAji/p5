ff926818884d3f9bb35e8a72ab5afb46e5b01f78
AjiNurAji <ajinuraji090306@gmail.com>
2025-08-20 09:38:26 +0700

    hide toggle layout in mobile

--- resources/js/pages/settings/appearance.tsx
+++ resources/js/pages/settings/appearance.tsx
@@ -1,35 +1,45 @@
-import { Head } from '@inertiajs/react';
+import { Head } from "@inertiajs/react";
 
-import AppearanceTabs from '@/components/appearance-tabs';
-import HeadingSmall from '@/components/heading-small';
-import { type BreadcrumbItem } from '@/types';
+import AppearanceTabs from "@/components/appearance-tabs";
+import HeadingSmall from "@/components/heading-small";
+import { type BreadcrumbItem } from "@/types";
 
-import AppLayout from '@/layouts/app-layout';
-import SettingsLayout from '@/layouts/settings/layout';
-import { LayoutToggleTab } from '@/components/layout-tabs';
+import { LayoutToggleTab } from "@/components/layout-tabs";
+import { useIsMobile } from "@/hooks/use-mobile";
+import AppLayout from "@/layouts/app-layout";
+import SettingsLayout from "@/layouts/settings/layout";
 
 const breadcrumbs: BreadcrumbItem[] = [
-    {
-        title: 'Pengaturan Tampilan',
-        href: '/settings/appearance',
-    },
+  {
+    title: "Pengaturan Tampilan",
+    href: "/settings/appearance",
+  },
 ];
 
 export default function Appearance() {
-    return (
-        <AppLayout breadcrumbs={breadcrumbs}>
-            <Head title="Pengaturan Tampilan" />
+  const isMobile = useIsMobile();
+  return (
+    <AppLayout breadcrumbs={breadcrumbs}>
+      <Head title="Pengaturan Tampilan" />
 
-            <SettingsLayout>
-                <div className="space-y-6">
-                    <HeadingSmall title="Tema" description="Pilih tema tampilan yang sesuai dengan preferensi Anda." />
-                    <AppearanceTabs />
-                </div>
-                <div className="space-y-6">
-                    <HeadingSmall title="Tata Letak" description="Pilih tata letak tampilan yang sesuai dengan preferensi Anda." />
-                    <LayoutToggleTab />
-                </div>
-            </SettingsLayout>
-        </AppLayout>
-    );
+      <SettingsLayout>
+        <div className="space-y-6">
+          <HeadingSmall
+            title="Tema"
+            description="Pilih tema tampilan yang sesuai dengan preferensi Anda."
+          />
+          <AppearanceTabs />
+        </div>
+        {!isMobile && (
+          <div className="space-y-6">
+            <HeadingSmall
+              title="Tata Letak"
+              description="Pilih tata letak tampilan yang sesuai dengan preferensi Anda."
+            />
+            <LayoutToggleTab />
+          </div>
+        )}
+      </SettingsLayout>
+    </AppLayout>
+  );
 }
